2111ca26640f817290e7ae3094e2aeeacdd1bb9b
dgriessler <32967199+dgriessler@users.noreply.github.com>
2019-11-13 23:15:34 -0500

    Added comments to DG's code

--- src/App/MusicContainer/Drawer.js
+++ src/App/MusicContainer/Drawer.js
@@ -1,44 +1,78 @@
+/**
+ * Keeps track of current note and where to draw it on the screen along with special information such as number of extra ledger lines
+ */
 class Drawer {
+    /**
+     * Creates a new Drawer setting up storage of the most recent midi note and information about how to draw it on the screen
+     * @param {Number} topLine Height of the top line of the selected part to sing
+     * @param {Number} distanceBetweenLines Distance between lines in the staff
+     */
     constructor(topLine, distanceBetweenLines) {
         this.topLine = topLine;
         this.distanceBetweenLines = distanceBetweenLines;
+        // stores the height of the lowest line of the staff being sung
         this.firstLine = this.topLine + this.distanceBetweenLines * 5;
-        this.lowerLimit = 61;
-        this.upperLimit = 81;
-        this.lowerLimit2 = 40;
-        this.upperLimit2 = 60;
+        // Values >= selected lower limit and <= selected upper limit don't need extra ledger lines
+        this.lowerLimit = 61; // 61 = C4#
+        this.upperLimit = 81; // 81 = A5
+        this.lowerLimit2 = 40; // 40 = E2
+        this.upperLimit2 = 60; // 60 = C4
         this.note = new Note(60);
         this.belowOrAbove = 0;
         this.noteHeight = 0;
         this.updateNote(this.note.midiVal);
     }
 
+    /**
+     * Updates the Drawer to the new provided note
+     * @param {Number} note New midi value to store. Provide a -1 as a sentinel value for silence 
+     */
     updateNote(note) {
         this.note.updateNote(note);
         this.getHeightOfNote();
         this.getExtraFeatures();
     }
 
+    /**
+     * Updates the height of the note based on its midi value
+     */
     getHeightOfNote() {
+        // -1 is a sentinel value for silence which is assigned a default height
         if (this.note.midiVal === -1) {
             this.noteHeight = this.firstLine;
             return;
         }
-        let heightMod = [0, 0, 1, 1, 2, 3, 3, 4, 4, 5, 5, 6];
+        // Calculating the height of a note relies on the cycle in musical notes that occurs between octaves
+        // This calculates what the height of the note should be based on the first line
+        const heightMod = [0, 0, 1, 1, 2, 3, 3, 4, 4, 5, 5, 6];
+
+        // C4 is the starting note so subtract 4 to get base octave
         let octaveMod = this.note.octave - 4;
         let value = heightMod[this.note.midiVal % heightMod.length];
+
+        // Includes bump to jump between octaves
         let totalMod = value + octaveMod * 7;
-        this.noteHeight = this.firstLine - (totalMod * this.distanceBetweenLines) / 2;
-        //this.noteHeight = this.firstLine;
+
+        // final height includes division by 2 because each value in the totalMod is distanceBetweenLines/2 
+        this.noteHeight = this.firstLine - (totalMod * this.distanceBetweenLines) / 2;      
     }
 
+    /**
+     * Gets the extra features of a note including how many ledger lines to add
+     */
     getExtraFeatures() {
+        // -1 is a sentinel value for silence which has no ledger lines
         if (this.note.midiVal === -1) {
             this.belowOrAbove = 0;
             return;
         }
-        let aboveBelowMod = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4];
 
+        // similar to note height, there's a cycle between octaves for ledger lines
+        const aboveBelowMod = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4];
+
+        // sets up base if ledger lines are even needed. base == 0 means no ledger lines
+        // base < 0 means they go below the staff, base > 0 means they go above the staff
+        // TODO Ensure this works with Bass clef
         let base = 0;
         if (this.note.midiVal >= this.upperLimit) {
             base = this.upperLimit;
@@ -46,12 +80,15 @@ class Drawer {
             base = -1 * this.lowerLimit;
         }
 
+        // If need ledger lines, then calculate how many are required
         if (base !== 0) {
             let difference = Math.abs(Math.abs(base) - this.note.midiVal);
             let loopAdd = 4 * Math.floor(difference / aboveBelowMod.length);
             let modAmount = difference % aboveBelowMod.length;
             modAmount = aboveBelowMod[modAmount];
             this.belowOrAbove = loopAdd + modAmount;
+
+            // Signals to draw ledger lines below staff
             if (base < 0) {
                 this.belowOrAbove *= -1;
             }
@@ -61,34 +98,59 @@ class Drawer {
     }
 }
 
+/**
+ * Stores midi value as its character representation including its octave and if it is sharp
+ */
 class Note {
+    /**
+     * Constructs a Note from a provided a given midiVal and converts it to a string which can be accessed
+     * @param {Number} midiVal Midi value of note to store
+     */
     constructor(midiVal) {
         this.updateNote(midiVal);
     }
 
+    /**
+     * Updates the note stored to the new note
+     * @param {Number} note New midi value to store
+     */
     updateNote(note) {
+        // No point in updating if the midi value matches the current one
         if (this.midiVal && note === this.midiVal) {
             return;
         }
+
         this.midiVal = note;
         const noteText = this.numToNote();
         this.charPart = noteText.charPart;
         this.octave = noteText.octave;
+
+        // relies on the char part with being a single letter like G or two letters which is the note and # for sharp
         this.isSharp = this.charPart.length === 2;
     }
 
+    /**
+     * Gets the octave of the current note
+     * @returns The octave of the current note
+     */
     getOctave() {
         return Math.floor(this.midiVal / 12) - 1;
     }
 
+    /**
+     * Converts the stored midi value to its character representation
+     * @returns A tuple with the character part and the octave
+     */
     numToNote() {
         let charPart;
         let octave;
+
+        // -1 is a sentinel value for silence which has no char part or octave
         if (this.midiVal === -1) {
             charPart = "-";
             octave = "";
         } else {
-            let letters = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+            const letters = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
             charPart = letters[this.midiVal % letters.length];
             octave = this.getOctave(this.midiVal);
         }
--- src/App/MusicContainer/NoteList.js
+++ src/App/MusicContainer/NoteList.js
@@ -1,6 +1,14 @@
 const MAX_SIZE = 5;
 
+/**
+ * Keeps a list of midi values to serve as a sample of the MAX_SIZE last midi values heard.
+ */
 class NoteList {
+
+    /**
+     * Creates a NoteList defaulting the lower pitch bound to 21 = A0 and the upper pitch bound to 127 = G9
+     * @param {Number} element Initial midi value to be stored in the list of values
+     */
     constructor(element) {
         this.elements = [element];
         this.pointer = 0;
@@ -10,7 +18,12 @@ class NoteList {
         this.upperPitchBound = 127;
     }
 
+    /**
+     * Adds a midi value to the list of values overwriting the oldest value if full
+     * @param {Number} element Midi value to be stored in the list of values
+     */
     addNote(element) {
+        // Adds element to the list overwriting the oldest value if full
         if (this.elements.length < MAX_SIZE) {
             this.elements.push(element);
         } else {
@@ -21,15 +34,23 @@ class NoteList {
         this.total += element;
         this.average = Math.round(this.total / this.elements.length);
 
+        // if the provided midi value is 0, this is the special value for silence
+        // Also display silence if the average is out of bounds
         if (
             element === 0 ||
             this.average < this.lowerPitchBound ||
             this.average > this.upperPitchBound
         ) {
+            // -1 is the sential value for silence to be diplayed
             this.average = -1;
         }
     }
 
+    /**
+     * Updates the lower and upper bounds on the average in terms of midi values
+     * @param {Number} lowerBound Averages less than this midi value will be ignored
+     * @param {Number} upperBound Averages greater than this midi value will be ignored
+     */
     updateBounds(lowerBound, upperBound) {
         this.lowerPitchBound = lowerBound;
         this.upperPitchBound = upperBound;
